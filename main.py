c1da54d1db212d73463c4fbc8e4f14d0624f61bf
Harsh Navdhare <navdhareharsh2001@gmail.com>
2021-10-23 18:42:02 +0530

    Fix Internal Serverl Error
    Signed-off-by: Harsh Navdhare <navdhareharsh2001@gmail.com>

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from telethon.sync import TelegramClient
 TOKEN: str = getenv("TOKEN", "")
 API_ID: int = int(getenv("API_ID", "0"))
 API_HASH: str = getenv("API_HASH", "")
-client = TelegramClient("bot", API_ID, API_HASH).start(bot_token=TOKEN)
+client = TelegramClient("bot", API_ID, API_HASH)
 app = FastAPI(title="Telegram ChatID to Chat Members API")
 
 
 @app.get("/{chat_id}", response_class=UJSONResponse)
 async def mention_list(chat_id: int):
-    chat_members = await client.get_participants(chat_id)
+    chat_members = None
+    my_client = await client.start(bot_token=TOKEN)
+    async with my_client:
+        chat_members = await my_client.get_participants(chat_id)
     return {user.id: user.first_name for user in chat_members}
